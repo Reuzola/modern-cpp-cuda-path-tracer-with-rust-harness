6e2e2dd406d4a52ab4e232ea12d8fa32c29acd05
Reuzola <aliozuer@gmail.com>
2026-07-08 20:54:00 +0300

    feat: add 3D vector class with basic math operators

---
+++ include/vec3.hpp
@@ -0,0 +1,89 @@
+#pragma once
+#include <cmath>
+
+class vec3 {
+
+private:
+    double e[3];
+
+public:
+    vec3() : e{0,0,0} {}
+    vec3(double a, double b, double c) : e{a,b,c} {}
+
+    double x() const { return e[0]; }
+    double y() const { return e[1]; }
+    double z() const { return e[2]; }
+
+    vec3 operator-() const { return vec3(-e[0], -e[1], -e[2]); }
+
+    double  operator[](int i) const { return e[i]; }
+    double& operator[](int i) { return e[i]; }
+
+    vec3& operator+=(const vec3& v) {
+        e[0] += v.e[0];
+        e[1] += v.e[1];
+        e[2] += v.e[2];
+        return *this;
+    }
+
+    vec3& operator*=(double t) {
+        e[0] *= t;
+        e[1] *= t;
+        e[2] *= t;
+        return *this;
+    }
+
+    vec3& operator/=(double t) {
+        return *this *= 1.0/t;
+    }
+
+    double length_squared() const {
+        return e[0]*e[0] + e[1]*e[1] + e[2]*e[2];
+    }
+
+    double length() const {
+        return std::sqrt(length_squared());
+    }
+};
+
+inline vec3 operator+(const vec3& u, const vec3& v) {
+    return vec3(u.x() + v.x(), u.y() + v.y(), u.z() + v.z());
+}
+
+inline vec3 operator-(const vec3& u, const vec3& v) {
+    return vec3(u.x() - v.x(), u.y() - v.y(), u.z() - v.z());
+}
+
+inline vec3 operator*(const vec3& u, const vec3& v) {
+    return vec3(u.x() * v.x(), u.y() * v.y(), u.z() * v.z());
+}
+
+inline vec3 operator*(double t, const vec3& v) {
+    return vec3(t * v.x(), t * v.y(), t * v.z());
+}
+
+inline vec3 operator*(const vec3& v, double t) {
+    return t * v;
+}
+
+inline vec3 operator/(const vec3& v, double t) {
+    return (1.0/t) * v;
+}
+
+inline double dot(const vec3& u, const vec3& v) {
+    return u.x() * v.x() + u.y() * v.y() + u.z() * v.z();
+}
+
+inline vec3 cross(const vec3& u, const vec3& v) {
+    return vec3(
+        u.y()*v.z() - u.z()*v.y(),
+        u.z()*v.x() - u.x()*v.z(),
+        u.x()*v.y() - u.y()*v.x()
+    );
+}
+
+inline vec3 unit_vector(const vec3& v) {
+    return v / v.length();
+}
+
+using point3 = vec3;
