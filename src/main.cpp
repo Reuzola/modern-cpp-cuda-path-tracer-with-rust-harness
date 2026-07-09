3f6b7ea923c977a56b55becaf49791de3d3dfb8a
Reuzola <aliozuer@gmail.com>
2026-07-09 17:11:02 +0300

    feat: add ray-sphere intersection and render red sphere

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,19 @@
 #include "color.hpp"
 #include "ray.hpp"
 
+[[nodiscard]] bool hit_sphere(const point3& center, double radius, const ray& r) {
+    const auto oc = center - r.origin();
+    const double a = r.direction().length_squared();
+    const double b = 2 * dot(oc, r.direction());
+    const double c = oc.length_squared() - (radius * radius);
+    const double discriminant = b*b - 4*a*c;
+    return (discriminant >= 0);
+}
+
 color ray_color(const ray& r) {
+    if(hit_sphere(point3(0, 0, -1), 0.5, r)) {
+        return color(1, 0, 0);
+    }
     vec3 unit_direction = unit_vector(r.direction());
     auto a = 0.5 * (unit_direction.y() + 1.0);
     return (1.0 - a) * color(1.0, 1.0, 1.0) + a * color(0.5, 0.7, 1.0);
