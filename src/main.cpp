880d93b756072ecc085e04677edbedcd30a7d262
Reuzola <aliozuer@gmail.com>
2026-07-13 21:54:02 +0300

    feat: assemble final material showcase scene

--- src/main.cpp
+++ src/main.cpp
@@ -10,17 +10,16 @@
 int main() {
     const lambertian material_ground{ color(0.8, 0.8, 0.0) };
     const lambertian material_center{ color(0.1, 0.2, 0.5) };
-
-    const dielectric material_left{ 1.00 / 1.33 }; // Glass
-
+    const dielectric material_left{ 1.5 };
+    const dielectric material_bubble{ 1.0 / 1.5};
     const metal material_right{ color(0.8, 0.6, 0.2), 1.0 };
 
     hittable_list world;
 
-    world.add(std::make_shared<sphere>(point3(0.0, 0.0, -1.2), 0.5, &material_center));
     world.add(std::make_shared<sphere>(point3(0.0, -100.5, -1.0), 100.0, &material_ground));
-
+    world.add(std::make_shared<sphere>(point3(0.0, 0.0, -1.2), 0.5, &material_center));
     world.add(std::make_shared<sphere>(point3(-1.0, 0.0, -1.0), 0.5, &material_left));
+    world.add(std::make_shared<sphere>(point3(-1.0, 0.0, -1.0), 0.4, &material_bubble));
     world.add(std::make_shared<sphere>(point3( 1.0, 0.0, -1.0), 0.5, &material_right));
 
     camera cam;
@@ -28,6 +27,7 @@ int main() {
     cam.aspect_ratio = 16.0 / 9.0;
     cam.image_width = 400;
     cam.samples_per_pixel = 100;
+    cam.max_depth = 50;
 
     cam.render(world);
 
