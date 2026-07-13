151572919bd6195129a36d23c7ed4e6c079a7bb1
Reuzola <aliozuer@gmail.com>
2026-07-13 16:36:05 +0300

    feat: handle total internal reflection in dielectric

--- include/dielectric.hpp
+++ include/dielectric.hpp
@@ -3,6 +3,7 @@
 #include "hit_record.hpp"
 #include "vec3.hpp"
 #include "ray.hpp"
+#include <cmath>
 
 class dielectric : public material {
     public:
@@ -10,11 +11,18 @@ class dielectric : public material {
 
         [[nodiscard]] std::optional<scatter_record> scatter(const ray& r_in, const hit_record& rec) const override {
             const double ri = rec.front_face ? (1.0 / refraction_index) : refraction_index; // ri means refraction index ratio
+            const auto unit_direction = unit_vector(r_in.direction());
 
-            const auto normalized = unit_vector(r_in.direction());
-            const auto refracted_direction = refract(normalized, rec.normal, ri);
+            const auto cos_theta = std::fmin(dot(-unit_direction, rec.normal), 1.0);
+            const auto sin_theta = std::sqrt(1 - cos_theta * cos_theta);
 
-            return scatter_record{ .attenuation = color(1.0, 1.0, 1.0), .scattered = ray(rec.p, refracted_direction) };
+            const bool cannot_refract = ri * sin_theta > 1.0;
+
+            vec3 direction;
+            if (cannot_refract) direction = reflect(unit_direction, rec.normal);
+            else direction = refract(unit_direction, rec.normal, ri);
+
+            return scatter_record{ .attenuation = color(1.0, 1.0, 1.0), .scattered = ray(rec.p, direction) };
         }
     private:
         double refraction_index{};
--- src/main.cpp
+++ src/main.cpp
@@ -11,7 +11,7 @@ int main() {
     const lambertian material_ground{ color(0.8, 0.8, 0.0) };
     const lambertian material_center{ color(0.1, 0.2, 0.5) };
 
-    const dielectric material_left{ 1.5 }; // Glass
+    const dielectric material_left{ 1.00 / 1.33 }; // Glass
 
     const metal material_right{ color(0.8, 0.6, 0.2), 1.0 };
 
