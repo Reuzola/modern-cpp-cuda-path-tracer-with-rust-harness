44517ad9b6305cc8025a0ec82664a69a05be70e8
Reuzola <aliozuer@gmail.com>
2026-07-20 11:35:49 +0300

    feat: switch perlin to gradient noise with random lattice vectors

--- include/noise_texture.hpp
+++ include/noise_texture.hpp
@@ -8,7 +8,7 @@ class noise_texture : public texture {
         explicit noise_texture(double scale) : scale(scale) {}
 
         [[nodiscard]] color value(double, double, const point3& p) const override {
-            return color(1.0, 1.0, 1.0) * noise.noise(scale * p);
+            return color(1.0, 1.0, 1.0) * 0.5 * (1.0 + noise.noise(scale * p));
         }
     private:
         perlin noise;
--- include/perlin.hpp
+++ include/perlin.hpp
@@ -9,7 +9,7 @@
 class perlin {
     public:
         perlin() {
-            std::ranges::generate(randfloat, [] { return random_double(); });
+            std::ranges::generate(randvec, [] { return unit_vector(vec3::random(-1, 1)); });
 
             std::iota(perm_x.begin(), perm_x.end(), 0);
             std::iota(perm_y.begin(), perm_y.end(), 0);
@@ -21,32 +21,28 @@ class perlin {
         }
 
         [[nodiscard]] double noise(const point3& p) const {
-            double u = p.x() - std::floor(p.x());
-            double v = p.y() - std::floor(p.y());
-            double w = p.z() - std::floor(p.z());
-
-            u = u*u*(3 - 2*u); // u = 3u² − 2u³
-            v = v*v*(3 - 2*v); // v = 3v² − 2v³
-            w = w*w*(3 - 2*w); // w = 3w² − 2w³
+            const double u = p.x() - std::floor(p.x());
+            const double v = p.y() - std::floor(p.y());
+            const double w = p.z() - std::floor(p.z());
 
             const int i = static_cast<int>(std::floor(p.x()));
             const int j = static_cast<int>(std::floor(p.y()));
             const int k = static_cast<int>(std::floor(p.z()));
 
-            double c[2][2][2];
+            vec3 c[2][2][2];
 
             for (int di = 0; di < 2; di++) {
                 for (int dj = 0; dj < 2; dj++) {
                     for (int dk = 0; dk < 2; dk++) {
-                        c[di][dj][dk] = randfloat[perm_x[(i+di) & 255] ^ perm_y[(j+dj) & 255] ^ perm_z[(k+dk) & 255]];
+                        c[di][dj][dk] = randvec[perm_x[(i+di) & 255] ^ perm_y[(j+dj) & 255] ^ perm_z[(k+dk) & 255]];
                     }
                 }
             }
-            return trilinear_interp(c, u, v, w);
+            return perlin_interp(c, u, v, w);
         }
     private:
         static constexpr int point_count{256};
-        std::array<double, point_count> randfloat;
+        std::array<vec3, point_count> randvec;
         std::array<int, point_count> perm_x, perm_y, perm_z;
 
         [[nodiscard]] static double trilinear_interp(const double c[2][2][2], double u, double v, double w) {
@@ -56,6 +52,23 @@ class perlin {
                 for (int j = 0; j < 2; j++) {
                     for (int k = 0; k < 2; k++) {
                         accum += (i*u + (1-i)*(1-u)) * (j*v + (1-j)*(1-v)) * (k*w + (1-k)*(1-w)) * c[i][j][k];
+                    }
+                }
+            }
+            return accum;
+        }
+
+        [[nodiscard]] static double perlin_interp(const vec3 c[2][2][2], double u, double v, double w) {
+            const double uu = u*u*(3 - 2*u); // u = 3u² − 2u³
+            const double vv = v*v*(3 - 2*v); // v = 3v² − 2v³
+            const double ww = w*w*(3 - 2*w); // w = 3w² − 2w³
+            
+            double accum{0};
+            for (int i = 0; i < 2; i++) {
+                for (int j = 0; j < 2; j++) {
+                    for (int k = 0; k < 2; k++) {
+                        const vec3 weight_v(u - i, v - j, w - k);
+                        accum += (i*uu + (1-i)*(1-uu)) * (j*vv + (1-j)*(1-vv)) * (k*ww + (1-k)*(1-ww)) * dot(c[i][j][k], weight_v);
                     }
                 }
             }
