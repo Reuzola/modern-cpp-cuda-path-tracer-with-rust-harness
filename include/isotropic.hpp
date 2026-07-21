a8ce789af5254c803cc1858cbf1da9d34c426729
Reuzola <aliozuer@gmail.com>
2026-07-21 19:15:53 +0300

    feat: add isotropic phase-function material

---
+++ include/isotropic.hpp
@@ -0,0 +1,17 @@
+#pragma once
+#include "material.hpp"
+#include "hit_record.hpp"
+#include "texture.hpp"
+#include "vec3.hpp"
+#include "ray.hpp"
+
+class isotropic : public material {
+    public:
+        explicit isotropic(const texture* tex) : tex(tex) {}
+
+        [[nodiscard]] std::optional<scatter_record> scatter(const ray& r_in, const hit_record& rec) const override {
+            return scatter_record{ .attenuation = tex->value(rec.u, rec.v, rec.p), .scattered = ray(rec.p, random_unit_vector(), r_in.time()) };
+        }
+    private:
+        const texture* tex;
+};
